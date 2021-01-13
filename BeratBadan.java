f40f991179501636680b79f02271d9e1d4f7a79f
Husnan <57633103+husnan622@users.noreply.github.com>
2021-01-13 21:19:44 +0700

    Add files via upload

---
+++ BeratBadan.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author asus
+ */
+public class BeratBadan {
+    public static void main(String[] args) {
+        FormBeratBadan fbb = new FormBeratBadan();
+        fbb.setVisible(true);
+    }
+}
